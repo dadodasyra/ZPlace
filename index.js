c282898117876ddf673fef93ad7ec2a57dbafcb8
dadodasyra <dadodasyraminecraftien@gmail.com>
2022-09-09 23:31:06 +0200

    Current color verification (local based)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let fs = require('fs'),
     PNG = require('pngjs').PNG;
-let XMLHttpRequest = require('xhr2');
+const XMLHttpRequest = require('xhr2');
 
 const config = require('./config.json');
 
+let currentColorMapping = [];
 const url = "https://place-api.zevent.fr/graphql";
 let totalPrice = 0;
 
@@ -30,13 +31,29 @@ fs.createReadStream(config.imagePath)
         xhrColor.setRequestHeader("Content-Type", "application/json");
         xhrColor.send('{"operationName":"getAvailableColors","variables":{},"query":"query getAvailableColors {\\n  getAvailableColors {\\n    colorCode\\n    name\\n    __typename\\n  }\\n}"}');
 
-        xhrColor.onreadystatechange = function () {
+        xhrColor.onreadystatechange = async function () {
             if (xhrColor.readyState === 4) {
                 console.log(JSON.parse(xhrColor.responseText))
                 let colors = JSON.parse(xhrColor.responseText).data.getAvailableColors;
-                let colorsRGB = colors.map((color) => {
+                let colorsRGB = await colors.map((color) => {
                     return hexToRgb(color.colorCode);
                 });
+
+                if (config.currentImage !== "") {
+                    await fs.createReadStream(config.currentImage)
+                        .pipe(new PNG())
+                        .on('parsed', function () {
+                            console.log("Parsing global image")
+                            for (let y = 0; y < this.height; y++) {
+                                for (let x = 0; x < this.width; x++) {
+                                    if (x % 100 === 0 || y % 100 === 0) console.log("Parsing global image : x" + x + " y" + y)
+                                    let idx = (this.width * y + x) << 2;
+                                    currentColorMapping[idx] = {r: this.data[idx], g: this.data[idx + 1], b: this.data[idx + 2]};
+                                }
+                            }
+                            console.log("Parsing global image done")
+                        });
+                }
 
                 for (let y = 0; y < height; y++) {
                     for (let x = 0; x < width; x++) {
@@ -58,8 +75,6 @@ fs.createReadStream(config.imagePath)
                                     let y = pixelObject.y;
                                     let idx = (width * y + x) << 2;
 
-                                    totalPrice += level;
-
                                     let xmlHttpRequest = new XMLHttpRequest();
                                     xmlHttpRequest.open("POST", url);
 
@@ -72,24 +87,22 @@ fs.createReadStream(config.imagePath)
                                         }
                                     }
 
-                                    //find the closest color in the palette colorsRGB which contains {{r,g,b}}
-                                    let closestColor = colorsRGB.reduce((prev, curr) => {
-                                        return (getDistance(prev, imgData[idx], imgData[idx + 1], imgData[idx + 2]) < getDistance(curr, imgData[idx], imgData[idx + 1], imgData[idx + 2]) ? prev : curr)
-                                    });
+                                    let rgb = {r: imgData[idx], g: imgData[idx + 1], b: imgData[idx + 2]};
+                                    let closestColorIndex = getClosestColorIndex(idx, colorsRGB, rgb);
+                                    let currentColor = currentColorMapping[idx];
 
-                                    //find closestColor index in colorsRGB which contains {{r,g,b}}
-                                    let closestColorIndex = colorsRGB.findIndex((color) => {
-                                        return color.r === closestColor.r && color.g === closestColor.g && color.b === closestColor.b;
-                                    }) + 1;
+                                    if (rgb["r"] !== currentColor["r"] && rgb["g"] !== currentColor["g"] && rgb["b"] !== currentColor["b"]) {
+                                        totalPrice += level;
 
-                                    console.log("Placing pixel at x " + x + " y " + y + " with level " + level + " and color " + closestColorIndex + ", current price : " + totalPrice);
-                                    data["variables"]["pixels"] = [{
-                                        "x": x,
-                                        "y": y,
-                                        "color": closestColorIndex,
-                                        "currentLevel": currentLevel
-                                    }];
-                                    if (config.placing) xmlHttpRequest.send(JSON.stringify(data));
+                                        console.log("Placing pixel at x " + x + " y " + y + " with level " + level + " and color " + closestColorIndex + ", current price : " + totalPrice);
+                                        data["variables"]["pixels"] = [{
+                                            "x": x,
+                                            "y": y,
+                                            "color": closestColorIndex,
+                                            "currentLevel": currentLevel
+                                        }];
+                                        if (config.placing) xmlHttpRequest.send(JSON.stringify(data));
+                                    } else console.log("Pixel at x " + x + " y " + y + " is already placed with color " + closestColorIndex+", current price : " + totalPrice);
                                 }
                             };
 
@@ -105,6 +118,18 @@ fs.createReadStream(config.imagePath)
             }
         };
     });
+
+function getClosestColorIndex(idx, colorsRGB, rgb) {
+    //find the closest color in the palette colorsRGB which contains {{r,g,b}}
+    let closestColor = colorsRGB.reduce((prev, curr) => {
+        return (getDistance(prev, rgb["r"], rgb["g"], rgb["b"]) < getDistance(curr, rgb["r"], rgb["g"], rgb["b"]) ? prev : curr)
+    });
+
+    //find closestColor index in colorsRGB which contains {{r,g,b}}
+    return colorsRGB.findIndex((color) => {
+        return color.r === closestColor.r && color.g === closestColor.g && color.b === closestColor.b;
+    }) + 1;
+}
 
 function getDistance(color1, r, g, b) {
     return Math.sqrt(Math.pow(color1.r - r, 2) + Math.pow(color1.g - g, 2) + Math.pow(color1.b - b, 2));
