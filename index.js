720a3ee2bd7032fcab57c9a0f318a8cc8f829b53
dadodasyra <dadodasyraminecraftien@gmail.com>
2022-09-10 15:34:34 +0200

    Add dynamic map getting from the server
    Remove a useless setting
    Add info on currentImage which is now automatically fetched from internet
    Add request dependency
    Reformat code in multiple functions to upgrade readibility (still ugly)
    Change zplace.png template which is now 700x500

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let fs = require('fs'),
-    PNG = require('pngjs').PNG;
+    PNG = require('pngjs').PNG,
+    request = require('request');
 const XMLHttpRequest = require('xhr2');
 
 const config = require('./config.json');
 
-let currentColorMapping = [];
 const url = "https://place-api.zevent.fr/graphql";
 let totalPrice = 0;
 
@@ -25,99 +25,154 @@ fs.createReadStream(config.imagePath)
         let width = this.width;
         let height = this.height;
 
-        let xhrColor = new XMLHttpRequest();
-        xhrColor.open("POST", url);
-        xhrColor.setRequestHeader("Accept", "application/json");
-        xhrColor.setRequestHeader("Content-Type", "application/json");
-        xhrColor.send('{"operationName":"getAvailableColors","variables":{},"query":"query getAvailableColors {\\n  getAvailableColors {\\n    colorCode\\n    name\\n    __typename\\n  }\\n}"}');
+        parseColors(imgData, width, height);
+    });
 
-        xhrColor.onreadystatechange = async function () {
-            if (xhrColor.readyState === 4) {
-                console.log(JSON.parse(xhrColor.responseText))
-                let colors = JSON.parse(xhrColor.responseText).data.getAvailableColors;
-                let colorsRGB = await colors.map((color) => {
-                    return hexToRgb(color.colorCode);
-                });
+let currentColorMapping = [];
 
-                if (config.currentImage !== "") {
-                    await fs.createReadStream(config.currentImage)
-                        .pipe(new PNG())
-                        .on('parsed', function () {
-                            console.log("Parsing global image")
-                            for (let y = 0; y < this.height; y++) {
-                                for (let x = 0; x < this.width; x++) {
-                                    if (x % 100 === 0 || y % 100 === 0) console.log("Parsing global image : x" + x + " y" + y)
-                                    let idx = (this.width * y + x) << 2;
-                                    currentColorMapping[idx] = {r: this.data[idx], g: this.data[idx + 1], b: this.data[idx + 2]};
-                                }
-                            }
-                            console.log("Parsing global image done")
+async function sleep(number) {
+    return new Promise(resolve => setTimeout(resolve, number));
+}
+
+function parseColors(imgData, width, height)
+{
+    let xhrColor = new XMLHttpRequest();
+    xhrColor.open("POST", url);
+    xhrColor.setRequestHeader("Accept", "application/json");
+    xhrColor.setRequestHeader("Content-Type", "application/json");
+    xhrColor.send('{"operationName":"getAvailableColors","variables":{},"query":"query getAvailableColors {\\n  getAvailableColors {\\n    colorCode\\n    name\\n    __typename\\n  }\\n}"}');
+
+    xhrColor.onreadystatechange = async function () {
+        if (xhrColor.readyState === 4) {
+            let colors = JSON.parse(xhrColor.responseText).data.getAvailableColors;
+            let colorsRGB = await colors.map((color) => {
+                return hexToRgb(color.colorCode);
+            });
+
+            if (config.currentImage === "map.png") {
+                console.log("Downloading ZPlace map image...");
+                //fetch map from internet
+                let xhrMap = new XMLHttpRequest();
+                xhrMap.open("POST", url);
+                xhrMap.setRequestHeader("Accept", "application/json");
+                xhrMap.setRequestHeader("Content-Type", "application/json");
+                xhrMap.send('{"operationName":"getLastBoardUrl","variables":{},"query":"query getLastBoardUrl {\\n  lastBoardUrl\\n}"}');
+                xhrMap.onreadystatechange = async function () {
+                    if (xhrMap.readyState === 4) {
+                        let url = JSON.parse(xhrMap.responseText).data.lastBoardUrl;
+                        console.log(url)
+                        await request.head(url, async function (err, res) {
+                            console.log('Map length', res.headers['content-length']);
+
+                            await request(url, async function () {
+                                console.log("Map downloaded");
+                                currentColorMapping = await loadMap();
+                                console.log("Map loaded with " + currentColorMapping.length + " pixels");
+
+                                askingPixels(height, width, imgData, colorsRGB);
+                            }).pipe(await fs.createWriteStream("map.png"));
                         });
-                }
-
-                for (let y = 0; y < height; y++) {
-                    for (let x = 0; x < width; x++) {
-                        let idx = (width * y + x) << 2;
-
-                        if (imgData[idx + 3] !== 0) {
-                            let currentLevel = 1;
-                            let xhr = new XMLHttpRequest();
-                            xhr.open("POST", url);
-                            xhr.setRequestHeader("Accept", "application/json");
-                            xhr.setRequestHeader("Content-Type", "application/json");
-
-                            xhr.onreadystatechange = function () {
-                                if (xhr.readyState === 4) {
-                                    let response = JSON.parse(xhr.responseText);
-                                    let pixelObject = response.data.getPixelLevel;
-                                    let level = pixelObject.level;
-                                    let x = pixelObject.x;
-                                    let y = pixelObject.y;
-                                    let idx = (width * y + x) << 2;
-
-                                    let xmlHttpRequest = new XMLHttpRequest();
-                                    xmlHttpRequest.open("POST", url);
-
-                                    xmlHttpRequest.setRequestHeader("Authorization", "Bearer " + config.token);
-                                    xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
-
-                                    xmlHttpRequest.onreadystatechange = function () {
-                                        if (xmlHttpRequest.readyState === 4) {
-                                            console.log("Response for x " + x + " y " + y + " with level " + level + " : " + xmlHttpRequest.responseText);
-                                        }
-                                    }
-
-                                    let rgb = {r: imgData[idx], g: imgData[idx + 1], b: imgData[idx + 2]};
-                                    let closestColorIndex = getClosestColorIndex(idx, colorsRGB, rgb);
-                                    let currentColor = currentColorMapping[idx];
-
-                                    if (rgb["r"] !== currentColor["r"] && rgb["g"] !== currentColor["g"] && rgb["b"] !== currentColor["b"]) {
-                                        totalPrice += level;
-
-                                        console.log("Placing pixel at x " + x + " y " + y + " with level " + level + " and color " + closestColorIndex + ", current price : " + totalPrice);
-                                        data["variables"]["pixels"] = [{
-                                            "x": x,
-                                            "y": y,
-                                            "color": closestColorIndex,
-                                            "currentLevel": currentLevel
-                                        }];
-                                        if (config.placing) xmlHttpRequest.send(JSON.stringify(data));
-                                    } else console.log("Pixel at x " + x + " y " + y + " is already placed with color " + closestColorIndex+", current price : " + totalPrice);
-                                }
-                            };
-
-                            let pixelLevelData = {
-                                "operationName": "getPixelLevel",
-                                "variables": {"pixel": {"x": x, "y": y}},
-                                "query": "query getPixelLevel($pixel: PixelUpgradeInput!) {\n  getPixelLevel(pixel: $pixel) {\n    x\n    y\n    level\n    coloredBy\n    upgradedBy\n    __typename\n  }\n}"
-                            };
-                            xhr.send(JSON.stringify(pixelLevelData));
-                        }
                     }
                 }
+            } else if (config.currentImage !== "") {
+                currentColorMapping = await loadMap();
+                console.log("Map loaded with " + currentColorMapping.length + " pixels");
+
+                askingPixels(height, width, imgData, colorsRGB);
             }
-        };
-    });
+        }
+    };
+}
+
+function askingPixels(height, width, imgData, colorsRGB)
+{
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            let idx = (width * y + x) << 2;
+
+            if (imgData[idx + 3] !== 0) { //pixel is not transparent
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", url);
+                xhr.setRequestHeader("Accept", "application/json");
+                xhr.setRequestHeader("Content-Type", "application/json");
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        parsingPixelResponse(xhr, width, imgData, colorsRGB);
+                    }
+                };
+
+                let pixelLevelData = {
+                    "operationName": "getPixelLevel",
+                    "variables": {"pixel": {"x": x, "y": y}},
+                    "query": "query getPixelLevel($pixel: PixelUpgradeInput!) {\n  getPixelLevel(pixel: $pixel) {\n    x\n    y\n    level\n    coloredBy\n    upgradedBy\n    __typename\n  }\n}"
+                };
+                xhr.send(JSON.stringify(pixelLevelData));
+            }
+        }
+    }
+
+}
+
+function parsingPixelResponse(xhr, width, imgData, colorsRGB)
+{
+    let response = JSON.parse(xhr.responseText);
+    let pixelObject = response.data.getPixelLevel;
+    let level = pixelObject.level;
+    let x = pixelObject.x;
+    let y = pixelObject.y;
+    let idx = (width * y + x) << 2;
+
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open("POST", url);
+
+    xmlHttpRequest.setRequestHeader("Authorization", "Bearer " + config.token);
+    xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+
+    xmlHttpRequest.onreadystatechange = function () {
+        if (xmlHttpRequest.readyState === 4) {
+            console.log("Response of placing for x " + x + " y " + y + " with level " + level + " : " + xmlHttpRequest.responseText);
+        }
+    }
+
+    let rgb = {r: imgData[idx], g: imgData[idx + 1], b: imgData[idx + 2]};
+    let closestColorIndex = getClosestColorIndex(idx, colorsRGB, rgb);
+    let currentColor = currentColorMapping[idx];
+
+    if (!currentColor || (rgb["r"] !== currentColor["r"] && rgb["g"] !== currentColor["g"] && rgb["b"] !== currentColor["b"])) {
+        totalPrice += level;
+
+        console.log("Placing pixel at x " + x + " y " + y + " with level " + level + " and color " + closestColorIndex + ", current price : " + totalPrice);
+        data["variables"]["pixels"] = [{
+            "x": x,
+            "y": y,
+            "color": closestColorIndex,
+            "currentLevel": level
+        }];
+        if (config.placing) xmlHttpRequest.send(JSON.stringify(data));
+    } else console.log("Pixel at x " + x + " y " + y + " is already placed with color " + closestColorIndex+", current price : " + totalPrice);
+}
+
+async function loadMap()
+{
+    let colorMap = [];
+
+    await fs.createReadStream(config.currentImage)
+        .pipe(new PNG())
+        .on('parsed', function () {
+            console.log("Parsing global image")
+            for (let y = 0; y < this.height; y++) {
+                for (let x = 0; x < this.width; x++) {
+                    if (x % 100 === 0 && y % 100 === 0 && y !== 0) console.log("Parsing global image : x" + x + " y" + y)
+                    let idx = (this.width * y + x) << 2;
+
+                    colorMap[idx] = {r: this.data[idx], g: this.data[idx + 1], b: this.data[idx + 2]};
+                }
+            }
+            console.log("Parsing global image done")
+        });
+    return colorMap
+}
 
 function getClosestColorIndex(idx, colorsRGB, rgb) {
     //find the closest color in the palette colorsRGB which contains {{r,g,b}}
